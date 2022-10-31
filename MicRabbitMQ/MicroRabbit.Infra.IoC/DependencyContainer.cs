024a4ae4539f5f6c3cfd6f06a3ccd2759a8e6e30
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-10-31 14:00:23 +0100

    Initial Commit of Infrastructure Dependency Project

---
+++ MicRabbitMQ/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicRabbitMQ.Domain.Core.Bus;
+using MiCRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
